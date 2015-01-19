5e62bccdeba1fce24aa85474675c057a72e846c6
Mike Donahue <mtdonahue1@gmail.com>
2015-01-19 18:11:26 -0500

    Fixed indents

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,7 +4,7 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-	if(obj === null) return "null";
+  if(obj === null) return "null";
   if(typeof(obj) === "function" || typeof(obj) === "undefined") return null;
   if(typeof(obj) === "number" || typeof(obj) === "boolean") return String(obj);
   if(typeof(obj) === "string") return '"' + obj + '"';
@@ -20,14 +20,14 @@ var stringifyJSON = function(obj) {
 
   // Object
   if(typeof(obj) === "object") {
-  	var result = [];
-  	var keys = Object.keys(obj);
-  	for (var i=0, length=keys.length; i<length; i++) {
-  		var value = stringifyJSON(obj[keys[i]]);
-  		if(value !== null) {
-  			result.push('"' + keys[i] + '":' + value);
-  		}
-  	}
-  	return "{" + result.join(",") + "}";
+    var result = [];
+    var keys = Object.keys(obj);
+    for (var i=0, length=keys.length; i<length; i++) {
+  	  var value = stringifyJSON(obj[keys[i]]);
+      if(value !== null) {
+  		  result.push('"' + keys[i] + '":' + value);
+  	  }
+    }
+    return "{" + result.join(",") + "}";
   }
 };
